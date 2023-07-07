843da63eb98999b5a5d62648dbbdc206e1cd92ff
theCephas <oshoiseoluwa@gmail.com>
2023-07-07 05:25:13 +0100

    Edited the styling and added my readme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -28,7 +28,7 @@ function App() {
     setIsTimeDisplayed(!isTimeDisplayed);
   }
   return (
-    <div className="container backdrop-opacity-20 backdrop-invert bg-white/30 sm:w-[500px] w-72 border mt-20 m-auto p-20 text-center rounded-xl h-96">
+    <div className="container backdrop-sepia-0 bg-neutral-100 bg-opacity-10 sm:w-[500px] w-72 shadow-lg shadow-transparent/10 mt-20 m-auto p-20 text-center rounded-xl h-96">
       <h1 className="sm:text-2xl text-xl tracking-wider font-bold text-gray-950 mt-8">{isTimeDisplayed ? timeWord : "TIME"}</h1>
       <button className="bg-gray-900 text-neutral-200 mt-20 p-4 rounded-2xl tracking-wide transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 tracking-wider" onClick={timeDisplay}>{isTimeDisplayed ? "Close Time" : "Get Time⏲️"}</button>
     </div>
